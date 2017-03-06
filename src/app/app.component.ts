bd794db6eec5a52fc96d410f0f63d277580b90dd
Mark <markmarkius@gmail.com>
2017-03-06 22:34:43 +0200

    add  date
    (cherry picked from commit 106b644)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 class Transaction{
   constructor(public name: string,
-              public value: number){}
+              public value: number,
+              public date: string){}
 }
 
 const earningsData: Transaction[] = [
-  {  name: 'name1', value: 250 },
-  {  name: 'name2', value: 400 }
+  {  name: 'name1', value: 250, date: '02.03.2017'},
+  {  name: 'name1', value: 250, date: '02.03.2017'},
+  {  name: 'name1', value: 250, date: '02.03.2017'},
+  {  name: 'name1', value: 250, date: '02.03.2017'},
+  {  name: 'name2', value: 400, date: '03.03.2017'}
 ];
 
 const costsData: Transaction[] = [
-  {  name: 'name1', value: 23 },
-  {  name: 'name2', value: 10 }
+  {  name: 'name1', value: 23, date: '04.03.2017'},
+  {  name: 'name2', value: 10, date: '05.03.2017'}
 ];
 
 @Component({
@@ -36,10 +40,13 @@ export class AppComponent implements OnInit {
   costsName: string = '';
   costsValue: string = '';
 
+  date: string = '';
+
   ngOnInit(){
     this.earnings = this.getSum(earningsData);
     this.costs = this.getSum(costsData);
     this.getRemainder();
+    this.date = this.getCurrentDate();
   }
 
   getSum(data) {
@@ -52,8 +59,21 @@ export class AppComponent implements OnInit {
     return sum;
   }
 
+  getCurrentDate(){
+    let currentDate = new Date();
+    let day, month, year, fullDate;
+
+    currentDate.getDate() < 10 ? day = '0' + currentDate.getDate() : day = currentDate.getDate();
+    (currentDate.getMonth()+1) < 10 ? month = '0' + (currentDate.getMonth()+1) : month = (currentDate.getMonth()+1);
+    year =  currentDate.getFullYear();
+
+    fullDate = day + '.' + month + '.' + year;
+
+    return fullDate;
+  }
+
   addEarnings() {
-    let newEarningsData: Transaction = new Transaction(this.earningsName, Number(this.earningsValue) );
+    let newEarningsData: Transaction = new Transaction(this.earningsName, Number(this.earningsValue), this.date);
     this.earningsData.push(newEarningsData);
 
     this.earnings = this.getSum(earningsData);
@@ -65,7 +85,7 @@ export class AppComponent implements OnInit {
   }
 
   addCosts(){
-    let newCostsData: Transaction = new Transaction(this.costsName, Number(this.costsValue) );
+    let newCostsData: Transaction = new Transaction(this.costsName, Number(this.costsValue), this.date );
     this.costsData.push(newCostsData);
 
     this.costs = this.getSum(costsData);
