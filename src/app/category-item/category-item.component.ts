e797275c47bc269fff45b9c7b9e45de994d1b3ac
Mark <markmarkius@gmail.com>
2017-03-17 23:35:55 +0200

    category item

--- src/app/category-item/category-item.component.ts
+++ src/app/category-item/category-item.component.ts
@@ -1,31 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs';
+import {Component, OnInit} from '@angular/core';
 
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location }               from '@angular/common';
-
+import {ActivatedRoute, Params} from '@angular/router';
+import {CATEGORIES} from '../app.mook';
 import {Category} from '../category';
 
-import {AppService} from '../app.service';
 
-
-import 'rxjs/add/operator/switchMap';
 @Component({
-  selector: 'app-category-item',
-  templateUrl: './category-item.component.html',
-  styleUrls: ['./category-item.component.css']
+    selector: 'app-category-item',
+    templateUrl: './category-item.component.html',
+    styleUrls: ['./category-item.component.css']
 })
 export class CategoryItemComponent implements OnInit {
-    category;
-    constructor(
-        private appService: AppService,
-        private route: ActivatedRoute,
-        private location: Location
-    ) {}
 
-  ngOnInit(): void {
-      this.route.params
-          .switchMap((params: Params) => this.appService.getData(+params['id']))
-          .subscribe(category => this.category = category);
-  }
+    category: Category;
+
+    constructor(private route: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.getCategory(+params['id']))
+            .subscribe(category => this.category = category);
+
+    }
+
+    getCategories(): Promise<Category[]> {
+        return Promise.resolve(CATEGORIES);
+    }
+
+    getCategory(id: number): Promise<Category> {
+        return this.getCategories()
+            .then(category => category.find(category => category.id === id));
+    }
+
 
 }
